f1dd70bb9968c6a03d3770072aea59ef86538d2e
kry8182 <91414467+kry8182@users.noreply.github.com>
2022-03-18 15:33:41 +0300

    Add files via upload

--- sql_miniscripts_cream.sql
+++ sql_miniscripts_cream.sql
@@ -24,9 +24,30 @@ select
 from (select 
 		t_tank03 as t
 	   from table1 where t_tank03 > 85
-	   and table1.current_time between timestamp '2022/03/14' and (timestamp '2022/03/14' + interval '1' day)  
+	   and table1.current_time between (timestamp '2022/03/14' - interval '1' day)  and (timestamp '2022/03/14')  
       ) tab
+-- и вставка его в таблицу
+insert into steril_integals_on_date( date, t3 ) 
+    select 
+        timestamp '2022/03/14',
+        avg(t)*count(t)*10 as t3_integral
+    from (select 
+        t_tank03 as t
+       from table1 where t_tank03 > 80
+       and table1.current_time between (timestamp '2022/03/14' - interval '1' day)  and timestamp '2022/03/14'  
+         ) tab
+ 
+--Среднее и максимум за период 
+select cast(max(t3) as numeric(7,0)), cast(avg(t3)as numeric(7,0)) from steril_integals_on_date
+where  date between (timestamp '2022/02/14'  and timestamp '2022/03/14' ;
 
+
+--Нахождение даты максимума интеграла стерилизации в 2021 году
+select date, max from 
+(select cast(max(t3) as numeric(7,0)) max, cast(avg(t3)as numeric(7,0)) from steril_integals_on_date s
+where  date between (timestamp '2021/01/01' - interval '1' day)  and timestamp '2022/01/01') tab
+join steril_integals_on_date s
+on cast(t3 as numeric(7,0)) = tab.max
 
 -- Вычисление отношения честного "интеграла" стерилизации к идеальному максимуму (90 град в течение 15 минут) для одного из танков
 select 
@@ -36,6 +57,8 @@ from (select
 	   from table1 where t_tank03 > 85
 	   and table1.current_time between (timestamp '2022/03/14' - interval '1' day) and current_date
       ) tab
+
+
 
 -- Вычисление нечестного "интеграла" стерилизации для двух танков
 select * from
