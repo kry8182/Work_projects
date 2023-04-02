2c342571611be928aa71dc311e53740f50e69af7
Alexey <91414467+kry8182@users.noreply.github.com>
2023-04-02 18:09:41 +0300

    Add files via upload

--- nord_ETL_journal.py
+++ nord_ETL_journal.py
@@ -1,369 +1,184 @@
+# Эталон на 21.03.23
 
--- Удаление строк начиная с некоего времени и установка в meta этого времени временем последнего изменения:
-DELETE FROM nord_1s WHERE time > to_timestamp('2023-03-17 05:41:56.062', 'YYYY-MM-DD HH24:MI:SS.MS' );
-update  meta_all set max_update_dt = to_timestamp('2023-03-21 08:16:10.783', 'YYYY-MM-DD HH24:MI:SS.MS' ) where table_name = 'nord_1s'; 
-
--- Просмотр конкретного отсчета:
-select * from nord_1s where time between to_timestamp('2023-03-21 00:00:01', 'YYYY-MM-DD HH24:MI:SS.MS' ) and to_timestamp('2023-03-21 00:00:02', 'YYYY-MM-DD HH24:MI:SS.MS' );
-
---grafana nord_1s с others:
-SELECT
- time AS "time",
- temperature_CIP  ,  
- temperature_na_vozvrate  ,  
- CASE when Alarm_bit then 50 else 0 end alarm_bit  , 
- pressure_CIP ,  
- -- CASE when potok < 50 then potok else null end potok  ,
- potok,  
- CASE when protok_CIP <= 120 then protok_CIP else null end vozvrat,
- temperature_CIP_2k   ,  
- temperature_na_vozvrate_2k   ,  
- pressure_CIP_2k  ,
- CASE when protok_CIP_2k <= 120 then protok_CIP_2k else null end vozvrat_2k    ,  
- -- CASE when potok_2k < 50 then potok_2k else null end potok_2k 
- potok_2k, 
- state,
- state_moika_2k,
- cast(null as real) as out_current_potok_2k_hz
-FROM nord_1s
-UNION 
-select 
-  time AS "time",
-  temperature_CIP  ,  
-  temperature_na_vozvrate  ,  
-  CASE when Alarm_bit then 50 else 0 end alarm_bit  , 
-  pressure_CIP ,  
---CASE when potok < 50 then potok else null end potok  ,
-  potok,  
-  CASE when protok_CIP <= 120 then protok_CIP else null end vozvrat,
-  temperature_CIP_2k   ,  
-  temperature_na_vozvrate_2k   ,  
-  pressure_CIP_2k  ,
-  CASE when protok_CIP_2k <= 120 then protok_CIP_2k else null end vozvrat_2k    ,  
---CASE when potok_2k < 50 then potok_2k else null end potok_2k 
-  potok_2k, 
-  null,
-  null,
-  null
-FROM nord_1min
-UNION
-select 
- time,
- null,
- null,
- null,
- null,
- null,
- null,
- null,
- null,
- null,
- null,
- null,
- null,
- null,
- out_current_potok_2k / 8000 * 50 
-from nord_other
-WHERE
--- time > to_timestamp('2023-03-25 05:41:56.062', 'YYYY-MM-DD HH24:MI:SS.MS' );
-  $__timeFilter(time) 
-ORDER BY 1
+import sys
+import pandas as pd
+import psycopg2
+import os
+import shutil
+# from datetime import datetime
+import datetime
 
 
---grafana nord_1s без others:
-SELECT
- time AS "time",
- temperature_CIP  ,  
- temperature_na_vozvrate  ,  
- CASE when Alarm_bit then 50 else 0 end alarm_bit  , 
- pressure_CIP ,  
- -- CASE when potok < 50 then potok else null end potok  ,
- potok,  
- CASE when protok_CIP <= 120 then protok_CIP else null end vozvrat,
- temperature_CIP_2k   ,  
- temperature_na_vozvrate_2k   ,  
- pressure_CIP_2k  ,
- CASE when protok_CIP_2k <= 120 then protok_CIP_2k else null end vozvrat_2k    ,  
- -- CASE when potok_2k < 50 then potok_2k else null end potok_2k 
- potok_2k, 
- state,
- state_moika_2k
- FROM nord_1s
-UNION ALL
-select 
-  time AS "time",
-  temperature_CIP  ,  
-  temperature_na_vozvrate  ,  
-  CASE when Alarm_bit then 50 else 0 end alarm_bit  , 
-  pressure_CIP ,  
---CASE when potok < 50 then potok else null end potok  ,
-  potok,  
-  CASE when protok_CIP <= 120 then protok_CIP else null end vozvrat,
-  temperature_CIP_2k   ,  
-  temperature_na_vozvrate_2k   ,  
-  pressure_CIP_2k  ,
-  CASE when protok_CIP_2k <= 120 then protok_CIP_2k else null end vozvrat_2k    ,  
---CASE when potok_2k < 50 then potok_2k else null end potok_2k 
-  potok_2k, 
-  null,
-  null
-FROM nord_1min
 
-WHERE
-  $__timeFilter(time) 
-ORDER BY 1;
-select 
- time,
- out_current_potok_2k / 8000 * 50 as out_current_potok_2k_hz
-from nord_other
-WHERE
-  $__timeFilter(time) 
-ORDER BY 1;
+def sql_script(path_script, date_string): # Функция запуска sql-скрипта
+    f = open(path_script,'r')
+    s = f.read()
+    sql = s.replace('[date]', date_string)
+    sql_coms = sql.replace('\n', ' ').split(';')[:-1]
+    for sql_com in sql_coms:
+        curs.execute(sql_com)
+    f.close()
 
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
 
---Графана nord_1s+1min JOIN  out_current_potok_2k:
+# Коннектимся с сервером
+print('=== ETL start ===')
+print('Connecting...')
+conn = create_connection("kip", "kip", "kip", "192.168.100.223", "5432")
+conn.autocommit = False
+curs = conn.cursor()
+print('Connected')
 
-SELEct 
- time AS "time",
- temperature_CIP  ,  
- temperature_na_vozvrate  ,  
- alarm_bit  , 
- pressure_CIP ,  
-potok,  
- vozvrat,
- temperature_CIP_2k   ,  
- temperature_na_vozvrate_2k   ,  
- pressure_CIP_2k  ,
- vozvrat_2k    ,  
-potok_2k, 
-state,
-state_moika_2k,
-out_current_potok_2k / 8000 * 50 as out_current_potok_2k_hz
- from
-(SELECT
-  time AS "time",
-    temperature_CIP  ,  
- temperature_na_vozvrate  ,  
--- Alarm_bit    ,
-CASE when Alarm_bit then 50 else 0 end alarm_bit  , 
- pressure_CIP ,  
--- CASE when potok < 50 then potok else null end potok  ,
-potok,  
-CASE when protok_CIP <= 120 then protok_CIP else null end vozvrat,
- temperature_CIP_2k   ,  
- temperature_na_vozvrate_2k   ,  
- pressure_CIP_2k  ,
--- Alarm_bit_2k ,  
-CASE when protok_CIP_2k <= 120 then protok_CIP_2k else null end vozvrat_2k    ,  
--- CASE when potok_2k < 50 then potok_2k else null end potok_2k 
-potok_2k, 
-state,
-state_moika_2k
-FROM nord_1s
-UNION 
-select 
-  time AS "time",
-  temperature_CIP  ,  
- temperature_na_vozvrate  ,  
--- Alarm_bit    ,
-CASE when Alarm_bit then 50 else 0 end alarm_bit  , 
- pressure_CIP ,  
--- CASE when potok < 50 then potok else null end potok  ,
-potok,  
-CASE when protok_CIP <= 120 then protok_CIP else null end vozvrat,
- temperature_CIP_2k   ,  
- temperature_na_vozvrate_2k   ,  
- pressure_CIP_2k  ,
--- Alarm_bit_2k ,  
-CASE when protok_CIP_2k <= 120 then protok_CIP_2k else null end vozvrat_2k    ,  
--- CASE when potok_2k < 50 then potok_2k else null end potok_2k 
-potok_2k, 
-null,
-null
-FROM nord_1min )t
-LEFT join (select time as tm, out_current_potok_2k from nord_other) o
-on o.tm between t.time - interval '500 milliseconds' and t.time + interval '500 milliseconds'
-WHERE
-1=1 
-and  $__timeFilter(time) 
--- and t.time > to_timestamp('2023-03-25 15:20:13.364', 'YYYY-MM-DD HH24:MI:SS.MS' )
--- And out_current_potok_2k is not null;
-ORDER BY 1
+#Считываем дату последней загрузки из таблицы метаданных
+curs.execute("select max_update_dt from meta_all where table_name = 'nord_journal'")
+date_from_meta = curs.fetchall()
+if len(date_from_meta) > 0:
+    t = date_from_meta[0][0] - pd.to_timedelta('3 hours') # В БД будем писать по местному времени, а в csv пишется по UTC.
+    #t = date_from_meta[0][0]
+    date_from_meta_str = t.strftime("%Y-%m-%d %H:%M:%S") 
+else: 
+    t = datetime.datetime(1900,1,1,0,0)
+    date_from_meta_str = t.strftime("%Y-%m-%d %H:%M:%S")
+print('max date in meta:', date_from_meta_str)
+date_start_search = t - pd.to_timedelta('1 day')
+str_date_start_search = date_start_search.strftime("%Y%m%d")
 
+# Получение путей к файлам 
 
--- grafana alkali:
-SELECT
-  time AS "time",
-  concentration_alk,
-  CASE when V114_open then 1 else 0 end V114_open  ,
-  CASE when V212_open then 1 else 0 end V212_open ,
-  CASE when V112_open then 1 else 0 end V112_open ,
-  CASE when V107 then 1 else 0 end V107 ,
-  CASE when h_urov_sheloch then 1 else 0 end h_urov_sheloch ,
-  CASE when alkali_not_ready then 1 else 0 end alkali_not_ready,
-  CASE when m_urov_sheloch then 1 else 0 end m_urov_sheloch,
-  n_moek_alkali / 10 as n_moek
-FROM nord_alkali
-UNION 
-select 
-  time AS "time",
-  concentration_alk,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null
-FROM nord_1min
-WHERE
-  $__timeFilter(time)
-ORDER BY 1
+file_found = False
+  # print(f'Reading files from {str_date_start_search} ...')
+path_source = r'D:/'
+dir_list = os.listdir(path_source)
+print(dir_list)
+files = []
+if 'AlarmLog.csv' in dir_list: 
+    file_found = True
+else:print (f'files are not found in {path_source}')
+if file_found:
+  file = path_source + 'AlarmLog.csv'
+  total_rows_counter = 0 #сумма всех вставленных строк
 
--- grafana concentration_vozvr + клапана возврата + в.у. щелочи
- 
-select 
-  time AS "time",
-  CAST (null as real) as concentration_vozvr,
-  CAST (null as real) as concentration_vozvr_2k,
-  CASE when V212_open then 1 else 0 end V212_open ,
-  CASE when V112_open then 1 else 0 end V112_open,
-  CASE when h_urov_sheloch then 1 else 0 end h_urov_sheloch 
-FROM nord_alkali
-UNION
-SELECT
-  time AS "time",
-  concentration_vozvr,
-  concentration_vozvr_2k,
-  null,
-  null,
-  null
-FROM nord_alkali
-WHERE
-  $__timeFilter(time)
-UNION 
-select 
-  time AS "time",
-  concentration_vozvr,
-  concentration_vozvr_2k,
-  null,
-  null,
-  null
-FROM nord_1min
-WHERE
-  $__timeFilter(time)
-ORDER BY 1
+  print(f'= start ETL for {file} : =')
 
---- определение момента неготовности щелочи (без использования журнала, через оконные функции)
-with alkali_window as (
-SELECT 
-    time,
-    alkali_not_ready,
-    concentration_alk,
-    n_moek_alkali,
-    V107,
-    lag(alkali_not_ready) over (order by time) as previous_not_ready,
-    lag(V107) over (order by time) as previous_podpitka,
-    lead(alkali_not_ready) over (order by time) as next_not_ready,
-    lag(n_moek_alkali) over (order by time) as previous_n_moek_alkali,
-    case when (lag(m_urov_sheloch) over (order by time))then 1 else 0 end as previous_m_urov_sheloch
-    
-FROM nord_alkali
-WHERE 1=1
-    and $__timeFilter(time)
--- and time > to_timestamp('2023-03-26 15:41:56.062', 'YYYY-MM-DD HH24:MI:SS.MS' )
-order by 1
-                   ) 
-                        
---Таблица по щелочным мойкам
+  # Считываем датафреймы из файлов:
+  df = pd.read_csv(file, sep = ',',encoding='utf-8')
+  
+  df.columns = ['time', 'name', 'Unnamed: 2', 'text','unnamed3','level','front','user']  
+#   print(df.columns.tolist())
+#   print(df)
+  df = df.dropna(subset=['time']) # чистим от NaN !df = df.astype(str)
+  df = df.astype(str)
+  df = df.reset_index(drop=True) #обновляем индексацию, чтобы не путались индескы удаленных dropna строк !
+  # max_update_dt = datetime.strptime(df['time'][len(df['time']) - 1],'%-d/%-m/%Y %-I:%M')
+  max_date_t = datetime.datetime.strptime(df['time'][0],'%m/%d/%Y %I:%M:%S %p')
+  # print(max_date_t, type(max_date_t))
 
-SELECT -- момент начала неготовности щелочи
-   time
-   ,concentration_alk
-   ,previous_n_moek_alkali
-   ,previous_m_urov_sheloch
-FROM 
-alkali_window
-WHERE 1=1
-and alkali_not_ready 
-and not previous_not_ready
-union all
-SELECT  --момент конца подпитки (минимум концентрации)
-   time 
-   ,concentration_alk
-   ,null
-   ,null
-FROM 
-alkali_window
-WHERE 1=1
-and not V107 
-and previous_podpitka
-union all
-SELECT -- момент конца приготовления
-   time 
-   ,concentration_alk
-   ,n_moek_alkali
-   ,null
-FROM 
-alkali_window
-WHERE 1=1
-and alkali_not_ready 
-and not next_not_ready;
+  
+#   print(df) 
+  print('Readed')
+  
+  print(f'max date in file: {max_date_t}')
+  if max_date_t < t: exit() # Переходим к следущему файлу, если последняя строчка в нем датируется раньше последней загрузки   
+  # 2. Загрузка новых данных
+  print('Loading changes to DWH...')
+  c = 0
+  print('Starting from:', df['time'][0])
+  for i in range (0,len(df['time'])):
+      if datetime.datetime.strptime(df['time'][i],'%m/%d/%Y %I:%M:%S %p') > t: 
+        if df['front'][i] == 'Alarm Raised': 
+            front = 1
+        elif df['front'][i] == 'Alarm Cleared' or df['front'][i] == 'Alarm Cleared (acknowledged)' : 
+            front = 0
+        else: front = 2
+        curs.execute(f'''
+          insert into nord_journal 
+      ( 
+         time   ,
+         name,
+         level,
+         front
+       ) 
+      values (
+      to_timestamp('{df['time'][i]}', 'MM/DD/YYYY HH12:MI:SS' ) + interval '3 hours',
+         '{df['name'][i]}',                                
+         '{df['level'][i]}',
+         '{front}'
+               ) 
+      on conflict do nothing
+                  ''' ) 
+        if front == 1:
+          curs.execute(f'''
+          insert into nord_journal 
+        (time, name, level, front)
+        values (
+        to_timestamp('{df['time'][i]}', 'MM/DD/YYYY HH12:MI:SS' ) + interval '3 hours' - interval '100 milliseconds',
+         '{df['name'][i]}',                                
+         '{df['level'][i]}',
+         0
+               )
+        on conflict do nothing
+                  ''' ) 
+        elif front == 0:
+          curs.execute(f'''
+          insert into nord_journal 
+        (time, name, level, front)
+        values (
+        to_timestamp('{df['time'][i]}', 'MM/DD/YYYY HH12:MI:SS' ) + interval '3 hours' - interval '100 milliseconds',
+         '{df['name'][i]}',                                
+         '{df['level'][i]}',
+         1
+               )
+        on conflict do nothing
+                  ''' ) 
+      if c % 1000 == 0: print(df['time'][i], end = '|')
+      c += 1
+  print(c,' rows inserted')
+  
+  
+  
+  # 4. ОБНОВЛЕНИЕ МЕТАДАННЫХ (скрипт meta.sql)
+  print('Updating metadata...')
+  curs.execute(f"""insert INTO meta_all ( table_name, max_update_dt ) 
+  values ('nord_journal', coalesce(to_timestamp( '{max_date_t}', 'YYYY-MM-DD HH24:MI:SS.MS' ), to_date( '1900.01.01', 'YYYY.MM.DD' ))) 
+  on conflict (table_name) do
+      update set max_update_dt = to_timestamp( '{max_date_t}', 'YYYY-MM-DD HH24:MI:SS.MS' )""")
+  print('Updated')  
+  # Фиксируем изменения
+  print('Commit')
+  conn.commit()  
+  # # Сохраняем файл в архив
+  # print('Saving file to archive...')
+  # folder = file[38:-13] # для 1s = [41:-13]
+  # if not os.path.exists('C:/ETL/nord/Архив/alarm_data/' + folder):
+  #     shutil.copytree('D:/Data Logging/Log Files/alarm_data/' + folder,'C:/ETL/nord/Архив/Analog_data_alarm/' + folder)
+  # else: 
+  #     shutil.copytree('D:/Data Logging/Log Files/alarm_data/' + folder,'C:/ETL/nord/Архив/Analog_data_alarm/' + folder + '_')
+  print(f'= stop ETL for {file} : =')    
+# #2. ПОСТРОЕНИЕ ОТЧЕТА 
+# print('Report creating...')
+# sql_script('', date)
+# print('Created')
 
+# # Фиксируем изменения в таблице отчетов
+# print('Commit')
+# conn.commit()
 
--- Изменение концентрации при последнем приготовлении
-with alkali_window as (
-SELECT 
-    time,
-    alkali_not_ready,
-    concentration_alk,
-    n_moek_alkali,
-    V107,
-    lag(alkali_not_ready) over (order by time) as previous_not_ready,
-    lag(V107) over (order by time) as previous_podpitka,
-    lead(alkali_not_ready) over (order by time) as next_not_ready,
-    lag(n_moek_alkali) over (order by time) as previous_n_moek_alkali,
-    case when (lag(m_urov_sheloch) over (order by time))then 1 else 0 end as previous_m_urov_sheloch
-    
-FROM nord_alkali
-WHERE 1=1
---     and $__timeFilter(time)
-    and time > to_timestamp('2023-03-26 15:41:56.062', 'YYYY-MM-DD HH24:MI:SS.MS' )
-order by 1
-                   ) 
-                        
---минимум концентрации во время пригтовления
-select max, min, max - min as padenie_conc  from
-(select  min(concentration_alk) from nord_alkali
-WHERE
-time between
-(SELECT -- момент начала неготовности щелочи
-   time
-FROM 
-alkali_window
-WHERE 1=1
-and alkali_not_ready 
-and not previous_not_ready) 
+# Закрываем коннект
+curs.close()
+conn.close()
+print('Connect closed')
 
-and 
-
-(SELECT -- момент конца приготовления
-   time 
-FROM 
-alkali_window
-WHERE 1=1
-and alkali_not_ready 
-and not next_not_ready) 
-) t1
-
-join
-
-(SELECT -- момент начала неготовности щелочи
-   max(concentration_alk)
-FROM 
-alkali_window
-WHERE 1=1
-and alkali_not_ready 
-and not previous_not_ready) t2
-on 1=1
+print('=== ETL complete ====')
