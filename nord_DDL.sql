21051a3b03ff6c49d90ef0e14d531031d3086762
Alexey <91414467+kry8182@users.noreply.github.com>
2023-04-02 18:10:03 +0300

    Add files via upload

--- nord_DDL.sql
+++ nord_DDL.sql
@@ -69,6 +69,29 @@ ADD COLUMN Potok_2k real,
 ADD COLUMN Show_0 boolean,
 ADD COLUMN Show_0_2k boolean;
 
+ALTER TABLE public.nord_1min
+    
+ADD COLUMN Temperature_CIP real ,  
+ADD COLUMN Temperature_na_vozvrate real ,  
+ADD COLUMN Alarm_bit  boolean  ,  
+ADD COLUMN Pressure_CIP real,  
+ADD COLUMN Potok  real  ,  
+ADD COLUMN Protok_CIP  real ,  
+ADD COLUMN Temperature_CIP_2k real   , 
+ADD COLUMN Temperature_na_vozvrate_2k real , 
+ADD COLUMN Pressure_CIP_2k  real,
+ADD COLUMN Alarm_bit_2k boolean,  
+ADD COLUMN Protok_CIP_2k  real  ,  
+ADD COLUMN Potok_2k real,
+ADD COLUMN Show_0 boolean,
+ADD COLUMN Show_0_2k boolean,
+ADD COLUMN Temperature_HotWoter real,
+ADD COLUMN Concentration_Alk real , 
+ADD COLUMN Concentration_Acid real, 
+ADD COLUMN Concentration_vozvr real, 
+ADD COLUMN Concentration_vozvr_2k real,
+ADD COLUMN Approx_val_T1 real;
+
 -- Или добавляем столбцы к созданной в pg_admin таблице только с первыми двумя столбцами (чтобы ее было видно в дереве)
 ALTER TABLE public.nord_1s
     
@@ -87,21 +110,66 @@ ADD COLUMN Potok_2k real,
 ADD COLUMN Show_0 boolean,
 ADD COLUMN Show_0_2k boolean;
 
+ALTER TABLE public.nord_1h
 
-Temperature_CIP  ,  
-Temperature_na_vozvrate  ,  
-Alarm_bit    ,  
-Pressure_CIP ,  
-Potok    ,  
-Protok_CIP   ,  
-Temperature_CIP_2k   ,  
-Temperature_na_vozvrate_2k   ,  
-Pressure_CIP_2k  ,
-Alarm_bit_2k ,  
-Protok_CIP_2k    ,  
-Potok_2k,
-Show_0,
-Show_0_2k
+ADD COLUMN Temperature_Acid real,
+ADD COLUMN Temperature_Alcali real,
+ADD COLUMN Concentration_gv real;
+
+ALTER TABLE public.nord_alkali
+ADD COLUMN Concentration_Alk real, 
+ADD COLUMN h_urov_sheloch	boolean,
+ADD COLUMN v112_open boolean,
+ADD COLUMN v212_open boolean,
+ADD COLUMN v114_open	boolean, 
+ADD COLUMN Temperature_Alcali	real,
+ADD COLUMN V107 boolean	,
+ADD COLUMN alkali_not_ready	boolean,
+ADD COLUMN Concentration_vozvr	real, 
+ADD COLUMN Concentration_vozvr_2k real;
+
+ALTER TABLE public.nord_alarm
+
+ADD COLUMN Alarm_bit	boolean, 
+ADD COLUMN Alarm_bit_2k boolean, 
+ADD COLUMN bit_sirena boolean	,
+ADD COLUMN bit_sirena_2k boolean	,
+ADD COLUMN alarm34	integer, 
+ADD COLUMN alarm35 integer	,
+ADD COLUMN alarm36 integer	,
+ADD COLUMN alarm37	integer,
+ADD COLUMN no_potok boolean	,
+ADD COLUMN no_vozvrat boolean	,
+ADD COLUMN Pvozd_MC boolean	,
+ADD COLUMN Pvozd_tank boolean	,
+ADD COLUMN avar_button	boolean,
+ADD COLUMN Exceeding_time_otkachki boolean	,
+ADD COLUMN Sboy_marshruta boolean	,
+ADD COLUMN Marshrut_ne_sobralsa boolean	,
+ADD COLUMN ban_coldwater boolean	,
+ADD COLUMN ban_hotwater boolean	,
+ADD COLUMN alarm34_2k	integer,
+ADD COLUMN alarm35_2k	integer,
+ADD COLUMN alarm36_2k integer	,
+ADD COLUMN alarm37_2k integer	,
+ADD COLUMN no_potok_2k boolean	,
+ADD COLUMN no_vozvrat_2k boolean,
+ADD COLUMN Exceeding_time_otkachki_2k boolean	,
+ADD COLUMN Sboy_marshruta_2k boolean	,
+ADD COLUMN Marshrut_ne_sobralsa_2k boolean
+
+ALTER TABLE public.nord_other
+
+ADD COLUMN Out_Current_potok_2k	real;
+
+ALTER TABLE public.nord_journal
+  ADD COLUMN name text,
+  ADD COLUMN level text,
+  ADD COLUMN front integer;
+
+
+
+
 
 ALTER TABLE public.nord
 
